872a8a83f07bd8f8768fbb0df2a42286fceb4210
OrelSto <aurelien.stolzenbach@gmail.com>
2023-09-12 12:00:21 +0200

    On branch main Your branch is up to date with 'origin/main'.
    Changes to be committed:
        modified:   .gitignore
        modified:   setup.py

--- setup.py
+++ setup.py
@@ -1,5 +1,11 @@
 from setuptools import setup, find_packages
 
+""" setup
+SUMMARY: setup the package with some infos
+
+EXTENDED SUMMARY: setup define commond variable usefull for the package, as its name, version, packages, and required packages.
+"""
+
 setup(
     name="Chemical Pathways Analysis Program",
     version="0.1.0",
