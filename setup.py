c98ec45293b113b531863284e93bb43561173b99
OrelSto <aurelien.stolzenbach@gmail.com>
2023-09-12 11:31:22 +0200

    On branch main Your branch is up to date with 'origin/main'.
    Changes to be committed:
        new file:   setup.py

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="Chemical Pathways Analysis Program",
+    version="0.1.0",
+    packages=find_packages(),
+    install_requires=[
+        # List your package's dependencies here
+    ],
+)
