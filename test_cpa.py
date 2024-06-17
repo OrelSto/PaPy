528881dbc7c47a89b7f9ccc7928e09c46a5f4fe4
OrelSto <aurelien.stolzenbach@gmail.com>
2024-06-17 13:03:59 +0200

    On branch development Your branch is up to date with 'origin/development'.
    Changes to be committed:
    	modified:   test_cpa.py

--- test_cpa.py
+++ test_cpa.py
@@ -13,3 +13,6 @@ cpa.run_cpa(timestep=1.0,rate_threshold=0.3e-9,t_min=1.0e4,target_species=['O2',
 # Test for another simple chemical model that includes Cl and ClO O3 destruction catalysis
 # This model is under dev since I have no clue of the real rates and proper concentration fo Cl and ClO
 # cpa.run_cpa(timestep=100.0,rate_threshold=1e-11,t_min=1.0e4,filename_model='user_model_O3destruction_example.txt',filename_concentration='user_concentration_O3destruction_example.txt',chronicle_writing=True)
+
+# test with Uranus data!
+cpa.run_cpa(timestep=1.0,rate_threshold=0.3e-9,t_min=1.0e4,target_species=['CH4','O2'],filename_model='reactions_Uranus.txt',filename_concentration='concentrations_Uranus.txt',chronicle_writing=True)
